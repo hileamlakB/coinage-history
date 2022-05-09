89ae2f0153c461a92e1be9e6d9d2ba1743feb94f
Hileamlak Yitayew <hileamlak@localhost.localdomain>
2022-05-09 13:29:53 -0700

    Updates

---
+++ precache-manifest.c444d195b516e83519ad3a01e16b73d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee36c09f48b01c12f05",
+    "url": "/coinage-history/static/css/main.85ab2d80.chunk.css"
+  },
+  {
+    "revision": "4ee36c09f48b01c12f05",
+    "url": "/coinage-history/static/js/main.4ee36c09.chunk.js"
+  },
+  {
+    "revision": "cf6b6bf545d5975ec723",
+    "url": "/coinage-history/static/js/1.cf6b6bf5.chunk.js"
+  },
+  {
+    "revision": "fc6b41434df11f5eebc6",
+    "url": "/coinage-history/static/js/2.fc6b4143.chunk.js"
+  },
+  {
+    "revision": "2356cec2f7040ad5dc16",
+    "url": "/coinage-history/static/js/runtime~main.2356cec2.js"
+  },
+  {
+    "revision": "f8dac13d95c0e2cd290e67d78548d2f6",
+    "url": "/coinage-history/static/media/coin.f8dac13d.jpg"
+  },
+  {
+    "revision": "4386f8e6a45e3ce8be677378831f2184",
+    "url": "/coinage-history/index.html"
+  }
+];
